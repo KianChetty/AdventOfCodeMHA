181000b73fec21c7cf45a269c3ff20a69f8d9cd1
Kian Chetty <kian.chetty@dotactiv.com>
2025-02-17 08:10:35 +0200

    Day 7 and 8

---
+++ DAY7/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;  
+
+class DAY7
+{
+    static void Main()
+    {
+        string[] equations = File.ReadAllLines("puzzleInput.txt");
+
+        BigInteger totalCalibrationResult = 0;
+
+        foreach (var equation in equations)
+        {
+            string[] parts = equation.Split(':');
+            BigInteger testValue = BigInteger.Parse(parts[0].Trim());
+            BigInteger[] numbers = parts[1].Trim().Split(' ').Select(BigInteger.Parse).ToArray();
+
+            if (CanEquationBeTrue(testValue, numbers))
+            {
+                totalCalibrationResult += testValue;
+            }
+        }
+
+        Console.WriteLine($"Total calibration result: {totalCalibrationResult}");
+    }
+
+    // Check if the equation can be made true using +, *, or ||
+    static bool CanEquationBeTrue(BigInteger testValue, BigInteger[] numbers)
+    {
+        int numOperators = numbers.Length - 1;
+        int totalCombinations = (int)Math.Pow(3, numOperators); // 3 operators: +, *, ||
+
+        for (int i = 0; i < totalCombinations; i++)
+        {
+            char[] operators = new char[numOperators];
+            int temp = i;
+
+            for (int j = 0; j < numOperators; j++)
+            {
+                int opType = temp % 3; // 3 possible operators
+                operators[j] = opType == 0 ? '+' : (opType == 1 ? '*' : '|'); // 0 = +, 1 = *, 2 = ||
+                temp /= 3;
+            }
+
+            BigInteger result = EvaluateExpression(numbers, operators);
+
+            if (result == testValue)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Evaluate the expression left to right, considering concatenation (||)
+    static BigInteger EvaluateExpression(BigInteger[] numbers, char[] operators)
+    {
+        BigInteger result = numbers[0];
+
+        for (int i = 0; i < operators.Length; i++)
+        {
+            if (operators[i] == '+')
+            {
+                result += numbers[i + 1];
+            }
+            else if (operators[i] == '*')
+            {
+                result *= numbers[i + 1];
+            }
+            else if (operators[i] == '|') 
+            {
+                result = BigInteger.Parse(result.ToString() + numbers[i + 1].ToString());
+            }
+        }
+
+        return result;
+    }
+}
---
+++ DAY8/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class DAY8
+{
+    static void Main()
+    {
+        string[] map = File.ReadAllLines("puzzleInput.txt");
+
+        // Find the positions of all antennas and their frequencies
+        List<(int x, int y, char frequency)> antennas = new List<(int, int, char)>();
+        for (int y = 0; y < map.Length; y++)
+        {
+            for (int x = 0; x < map[y].Length; x++)
+            {
+                if (map[y][x] != '.')
+                {
+                    antennas.Add((x, y, map[y][x]));
+                }
+            }
+        }
+
+        // Track unique antinode locations
+        HashSet<(int x, int y)> antinodes = new HashSet<(int, int)>();
+
+        // Iterate over all grid positions
+        for (int y = 0; y < map.Length; y++)
+        {
+            for (int x = 0; x < map[y].Length; x++)
+            {
+                if (IsAntinode(x, y, antennas))
+                {
+                    antinodes.Add((x, y));
+                }
+            }
+        }
+
+        Console.WriteLine($"Unique antinode locations: {antinodes.Count}");
+    }
+
+    // Check if a position is an antinode
+    static bool IsAntinode(int x, int y, List<(int x, int y, char frequency)> antennas)
+    {
+        // Group antennas by frequency
+        var frequencyGroups = antennas.GroupBy(a => a.frequency);
+
+        foreach (var group in frequencyGroups)
+        {
+            // Get all antennas of this frequency
+            var sameFrequencyAntennas = group.ToList();
+
+            // Check if this position is in line with at least two antennas of the same frequency
+            if (sameFrequencyAntennas.Count >= 2)
+            {
+                for (int i = 0; i < sameFrequencyAntennas.Count; i++)
+                {
+                    for (int j = i + 1; j < sameFrequencyAntennas.Count; j++)
+                    {
+                        if (AreCollinear(x, y, sameFrequencyAntennas[i], sameFrequencyAntennas[j]))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    // Check if a point (x, y) is collinear with two antennas
+    static bool AreCollinear(int x, int y, (int x, int y, char frequency) a, (int x, int y, char frequency) b)
+    {
+        int crossProduct = (a.x - x) * (b.y - y) - (a.y - y) * (b.x - x);
+        return crossProduct == 0;
+    }
+}
